016691de579bccaa54496b63b31db0f6e2d942e4
Варя <@TAICHI>
2019-03-15 12:24:03 +0300

    Add full chapter 14

---
+++ AppConfigReaderApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace AppConfigReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Reading <appSettings> Data ****");
+            AppSettingsReader sr = new AppSettingsReader();
+            int count = (int)sr.GetValue("Count", typeof(int));
+            string color = (string)sr.GetValue("TextColor", typeof(string));
+
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color);
+            for (int i = 0; i < count; i++)
+                Console.WriteLine("Settings succesfully read");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodeBaseClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace CodeBaseClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun With Code Base ****");
+            SportsCar viper = new SportsCar();
+            Console.WriteLine("SportsCar allocated");
+            Console.ReadLine();
+        }
+    }
+}
