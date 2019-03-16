4e9508be69938e74bd61739083756cddb35acb24
Варя <@TAICHI>
2019-03-16 18:45:38 +0300

    Finished chapter 16

---
+++ DynamicKeyword/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImplicitlyTypedVaewable();
+            PrintThreeStrings();
+            ChangeDynamicDataType();
+            InvokeMembersOnDynamicData();
+
+            Console.ReadLine();
+        }
+
+        private static void InvokeMembersOnDynamicData()
+        {
+            dynamic t = "hello";
+
+            try
+            {
+                Console.WriteLine(t.ToUpper());
+
+                Console.WriteLine(t.toupper());
+                Console.WriteLine(t.Foo(9, true, "string"));
+            }
+            catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+        }
+
+        private static void ChangeDynamicDataType()
+        {
+            dynamic t = "Hello";
+            Console.WriteLine(t.GetType());
+
+            t = true;
+            Console.WriteLine(t.GetType());
+
+            t = new List<int>();
+            Console.WriteLine(t.GetType());
+            Console.WriteLine();
+        }
+
+        private static void PrintThreeStrings()
+        {
+            var str1 = "";
+            object str2 = "";
+            dynamic str3 = "";
+            Console.WriteLine("str1 type: {0}", str1.GetType());
+            Console.WriteLine("str2 type: {0}", str2.GetType());
+            Console.WriteLine("str3 type: {0}", str3.GetType());
+            Console.WriteLine();
+        }
+
+        private static void ImplicitlyTypedVaewable()
+        {
+            var list = new List<int>();
+            list.Add(90);
+            //list = "";
+        }
+    }
+}
---
+++ LateBindingWithDynamic/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace LateBindingWithDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddWithReflection();
+            AddWithDynamic();
+
+            Console.ReadLine();
+        }
+
+        private static void AddWithDynamic()
+        {
+            Assembly asm = Assembly.Load("MathLibarary");
+            try
+            {
+                Type t = asm.GetType("MathLibarary.SimpleMath");
+                dynamic inst = Activator.CreateInstance(t);
+                Console.WriteLine("Result: {0}", inst.Add(3, 2));
+            }
+            catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void AddWithReflection()
+        {
+            Assembly asm = Assembly.Load("MathLibarary");
+            try
+            {
+                Type t = asm.GetType("MathLibarary.SimpleMath");
+                object inst = Activator.CreateInstance(t);
+                MethodInfo add = t.GetMethod("Add");
+                Console.WriteLine("Result: {0}", add.Invoke(inst, new Object[] { 3, 2 }));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
